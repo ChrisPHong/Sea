f89eb3784063735df1f9510469733d271c8e1e0c
christopherchueng <christopherchueng@gmail.com>
2022-06-25 14:58:49 -0400

    Created Transaction model

---
+++ app/models/transaction.py
@@ -0,0 +1,18 @@
+from .db import db
+
+class Transaction(db.Model):
+    __tablename__ = "transactions"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    price = db.Column(db.Float) # Price of ONE share
+    shares = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(10), nullable=False) # Buy or sell
+    date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False, db.ForeignKey('users.id'))
+    company_id = db.Column(db.Integer, nullable=False, db.ForeignKey('companies.id'))
+
+    # Many-to-One relationship with Users
+    users = db.relationship('User', back_populates='transactions')
+
+    # Many-to-One relationship with Companies
+    companies = db.relationship('Company', back_populates='transactions')
