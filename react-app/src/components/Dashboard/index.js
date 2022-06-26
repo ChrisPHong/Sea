7e4029b219657f35a5e109f02a140d9098d2fd69
christopherchueng <christopherchueng@gmail.com>
2022-06-26 16:25:33 -0400

    Imported into dashboard index

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import './Dashboard.css'
+
 const Dashboard = () => {
+    const dispatch = useDispatch()
+    const currentUser = useSelector(state => state.session.user);
+
+    useEffect(() => {
+
+    }, [dispatch])
+
     return (
         <h1>Hi from Dashboard!</h1>
     )
