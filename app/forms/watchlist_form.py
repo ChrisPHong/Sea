81be3ae80667a2a17b1280f956d309f248128b7e
Grace Chi <gracetrc7@gmail.com>
2022-06-26 13:26:06 -0500

    created watchlist_form

---
+++ app/forms/watchlist_form.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+from app.models import Watchlist
+from flask_login import current_user
+
+# name must be unique
+def watchlist_validator(field):
+    name = field.data
+    user_id = current_user.id
+    watchlists = Watchlist.query.filter(Watchlist.user_id == user_id).all()
+
+    for watchlist in watchlists:
+        if watchlist.name == name:
+            raise ValidationError(f"The watchlist '{name}' already exists.")
+
+class WatchlistForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired(), watchlist_validator])
