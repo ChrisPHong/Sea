efbd0e199f49b206e12b8bc7b8baaaea40712b77
Grace Chi <gracetrc7@gmail.com>
2022-06-26 14:26:48 -0500

    navbar frontend login/logout credentials

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,35 +1,38 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  const currentUser = useSelector(state => state.session.user);
+
   return (
     <nav>
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            Sea
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        {currentUser
+        ?
+          <li>
+            <LogoutButton />
+          </li>
+        :
+          <>
+            <li>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+        }
       </ul>
     </nav>
   );
