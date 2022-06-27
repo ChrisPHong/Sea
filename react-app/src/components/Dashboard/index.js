cc8a9c0617bf8291465b9ead2de3862014db030f
christopherchueng <christopherchueng@gmail.com>
2022-06-26 20:25:46 -0400

    Company tickers fetched from backend and now displays in frontend

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,17 +1,32 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getStocks } from '../../store/stock';
 import { getTransactions } from '../../store/transaction';
 import './Dashboard.css'
 
 const Dashboard = () => {
     const dispatch = useDispatch()
     const transactions = useSelector(state => state?.transaction?.entries)
+    const stocks = useSelector(state => state?.stock?.entries)
     const currentUser = useSelector(state => state?.session?.user);
     const transArr = Object.values(transactions)
 
     useEffect(() => {
         dispatch(getTransactions(currentUser?.id))
     }, [dispatch])
+
+    useEffect(() => {
+        dispatch(getStocks())
+    }, [dispatch])
+
+    const matchTicker = (companyId) => {
+        const companies = Object.values(stocks)
+        for (let stock of companies) {
+            if (stock.id === companyId) {
+                return stock.ticker
+            }
+        }
+    }
 
     return (
         <>
@@ -22,7 +37,7 @@ const Dashboard = () => {
                     ?
                     <li key={transaction.id}>
                         <div>
-                            Company: {transaction.companyId}
+                            Company: {matchTicker(transaction.companyId)}
                         </div>
                         <div>
                             Shares: {transaction.shares}
