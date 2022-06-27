50ab1bf5076ae63b9e12b0bbd0aa03bd7ebd6f11
justin <jyi@ucsb.edu>
2022-06-27 00:44:32 -0700

    Need to add font style

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,47 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='login-page'>
+      <div className='login-image'>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
+      <div className='login-form'>
+        <form className='login-form-inputs' onSubmit={onLogin}>
+          <div className='login-form-title'>
+            Log in to SeaCoin
+          </div>
+          <div>
+            <label className='login-email-input' htmlFor='email'>Email</label>
+            <input
+              className='email-form-field'
+              name='email'
+              type='text'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <label className='login-password-input' htmlFor='password'>Password</label>
+            <input
+              className='password-form-field'
+              name='password'
+              type='password'
+              value={password}
+              onChange={updatePassword}
+            />
+            <div className='login-form-errors'>
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
+            </div>
+            <button className='login-button' type='submit'>Log in</button>
+          </div>
+          <div className='login-form-signup-nav'>
+            Don't have an account?
+            <NavLink className='login-form-create-account' to='/sign-up'>Create an account</NavLink>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
