45ba739f56feb16cbf50997d27af978749d04b95
Grace Chi <gracetrc7@gmail.com>
2022-06-28 18:50:51 -0500

    added demo button to login page, starting transaction component

--- react-app/src/components/auth/AuthPageDemoBtn.js
+++ react-app/src/components/auth/AuthPageDemoBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { login } from '../../store/session';
-import '../SplashPage/splashpage.css';
+import './LoginForm.css';
 
 const DemoButton = () => {
     const dispatch = useDispatch()
@@ -16,7 +16,7 @@ const DemoButton = () => {
     }
 
     return (
-        <button className="splash-demo-btn" type='submit' onClick={handleSubmit}>Demo User</button>
+        <button className="auth-demo-btn" type='submit' onClick={handleSubmit}>Demo User</button>
     )
 }
 
