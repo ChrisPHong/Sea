22632159625c870dbc6f8654e2b88b8722d169f4
Grace Chi <gracetrc7@gmail.com>
2022-08-03 13:12:39 -0500

    404 page styling

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,24 +1,30 @@
 import './pagenotfound.css';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import seaLogo from './sea-icon.png';
 
 function PageNotFound() {
     const sessionUser = useSelector(state => state?.session?.user);
     return (
         <div className='page-not-found'>
-            <p id='page-not-found-title'>
-                Looks like the page you were looking for is not found!
-            </p>
-            <>
-                <p id='page-not-found-text'>
-                    Go back to <span> </span>
-                    { sessionUser ?
-                    <span id='back-to-home'><NavLink exact to='/dashboard'>your dashboard! </NavLink></span>
-                    :
-                    <span id='back-to-home'><NavLink exact to='/'>Sea Homepage! </NavLink></span>
-                    }
+            <div className='page-not-found-box'>
+                <img className='sea-logo-404' src={seaLogo} alt='sea-logo' />
+                <p className='page-not-found-title' id='first-p'>
+                    Looks like the page you were
                 </p>
-            </>
+                <p className='page-not-found-title' id='second-p'>
+                    looking for is not found!
+                </p>
+                <>
+                    <p id='page-not-found-text'>
+                        { sessionUser ?
+                        <span id='back-to-home'><NavLink exact to='/dashboard'>Back to Dashboard</NavLink></span>
+                        :
+                        <span id='back-to-home'><NavLink exact to='/'>Back to Sea Homepage</NavLink></span>
+                        }
+                    </p>
+                </>
+            </div>
         </div>
     )
 }
